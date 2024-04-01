a7fae49d6b703daf527ffb1c5a7056a324e57464
brosigm <marcibece@gmail.com>
2024-04-01 20:55:32 +0200

    fix

---
+++ api/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS checkplaces (
+    id SERIAL PRIMARY KEY,
+    location TEXT NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    user_id INTEGER NOT NULL,
+    link TEXT
+  )
+`;
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/status", (request, response) => {
+    const status = {
+        "Status": "Running"
+    };
+    response.send(status);
+});
+
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+})
+
+
+app.get("/checkplaces", async (request, response) => {
+    const { rows } = await pool.query('SELECT * FROM checkplaces');
+    response.send(rows);
+});
+
+app.post("/checkplaces", async (request, response) => {
+    const { location, date, time, user_id, link } = request.body;
+    const query = {
+        text: 'INSERT INTO checkplaces(location, date, time, user_id, link) VALUES($1, $2, $3, $4, $5)',
+        values: [location, date, time, user_id, link],
+    };
+    await pool.query(query);
+    response.send("Data Inserted Successfully");
+});
+
+app.get("/checkplaces/:id", async (request, response) => {
+    const { id } = request.params;
+    const { rows } = await pool.query('SELECT * FROM checkplaces WHERE id = $1', [id]);
+    response.send(rows);
+});
+
+
+
